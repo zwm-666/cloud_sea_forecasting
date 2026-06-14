f66799773224df6d36dee4c46a9555682ef9c988
zwm-666 <zwm7756@gmail.com>
2026-06-14 14:48:42 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,19 @@
+// app.js
+App({
+  onLaunch: function () {
+    this.globalData = {
+      // env 参数说明：
+      // env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会请求到哪个云环境的资源
+      // 此处请填入环境 ID, 环境 ID 可在微信开发者工具右上顶部工具栏点击云开发按钮打开获取
+      env: "",
+    };
+    if (!wx.cloud) {
+      console.error("请使用 2.2.3 或以上的基础库以使用云能力");
+    } else {
+      wx.cloud.init({
+        env: this.globalData.env,
+        traceUser: true,
+      });
+    }
+  },
+});
